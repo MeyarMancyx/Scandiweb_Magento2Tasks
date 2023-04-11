7d74c1ba0ae7f9ff087c2628d7d393313030dc11
Meyar Mancy <meyarmancy@Meyars-MacBook-Pro.local>
2023-04-11 14:27:21 +0200

    final commit 2

--- app/design/frontend/Scandiweb/base/Magento_Checkout/web/js/sidebar-mixin.js
+++ app/design/frontend/Scandiweb/base/Magento_Checkout/web/js/sidebar-mixin.js
@@ -1,96 +1,96 @@
 define([
-    'jquery',    
+    'jquery',
     'uiComponent'
-], function($, Component) {
-    'use strict'; 
-    return function(target) {
-    return $.widget('mage.sidebar', $.mage.sidebar, {
-        options: {
-            isRecursive: true,
-            item: {
-                'qtybox':':input.cart-item-qtybox',
-            }
-        },       
-
-        /**
-         * @private
-         */
-        _initContent: function () {
-            var self = this,
-                events = {};
-
-            this.element.decorate('list', this.options.isRecursive);
-
-            /**
-             * @param {jQuery.Event} event
-             */
-            events['click ' + this.options.button.close] = function (event) {
-                event.stopPropagation();
-                $(self.options.targetElement).dropdownDialog('close');
-            };
-            events['click ' + this.options.button.checkout] = $.proxy(function () {
-                var cart = customerData.get('cart'),
-                    customer = customerData.get('customer'),
-                    element = $(this.options.button.checkout);
-
-                if (!customer().firstname && cart().isGuestCheckoutAllowed === false) {
-                    // set URL for redirect on successful login/registration. It's postprocessed on backend.
-                    $.cookie('login_redirect', this.options.url.checkout);
-
-                    if (this.options.url.isRedirectRequired) {
-                        element.prop('disabled', true);
-                        location.href = this.options.url.loginUrl;
-                    } else {
-                        authenticationPopup.showModal();
-                    }
-
-                    return false;
+], function ($, Component) {
+    'use strict';
+    return function (target) {
+        return $.widget('mage.sidebar', $.mage.sidebar, {
+            options: {
+                isRecursive: true,
+                item: {
+                    'qtybox': ':input.cart-item-qtybox',
                 }
-                element.prop('disabled', true);
-                location.href = this.options.url.checkout;
-            }, this);
+            },
 
             /**
-             * @param {jQuery.Event} event
+             * @private
              */
-            events['click ' + this.options.button.remove] =  function (event) {
-                event.stopPropagation();
-                self._removeItem($(event.currentTarget));
-            };
+            _initContent: function () {
+                var self = this,
+                    events = {};
 
-            /**
-             * @param {jQuery.Event} event
-             */
-            events['keyup ' + this.options.item.qty] = function (event) {
-                self._showItemButton($(event.target));
-            };
+                this.element.decorate('list', this.options.isRecursive);
 
-            /**
-             * @param {jQuery.Event} event
-             */
-            events['change ' + this.options.item.qty] = function (event) {
-                self._showItemButton($(event.target));
-            };
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['click ' + this.options.button.close] = function (event) {
+                    event.stopPropagation();
+                    $(self.options.targetElement).dropdownDialog('close');
+                };
+                events['click ' + this.options.button.checkout] = $.proxy(function () {
+                    var cart = customerData.get('cart'),
+                        customer = customerData.get('customer'),
+                        element = $(this.options.button.checkout);
 
-            /**
-             * @param {jQuery.Event} event
-             */
-            events['click ' + this.options.item.button] = function (event) {
-                event.stopPropagation();
-                self._updateItemQty($(event.currentTarget));
-            };
+                    if (!customer().firstname && cart().isGuestCheckoutAllowed === false) {
+                        // set URL for redirect on successful login/registration. It's postprocessed on backend.
+                        $.cookie('login_redirect', this.options.url.checkout);
 
-            /**
-             * @param {jQuery.Event} event
-             */
-            events['focusout ' + this.options.item.qty] = function (event) {
-                self._validateQty($(event.currentTarget));
-            };
+                        if (this.options.url.isRedirectRequired) {
+                            element.prop('disabled', true);
+                            location.href = this.options.url.loginUrl;
+                        } else {
+                            authenticationPopup.showModal();
+                        }
 
-            this._on(this.element, events);
-            this._calcHeight();
-        },
+                        return false;
+                    }
+                    element.prop('disabled', true);
+                    location.href = this.options.url.checkout;
+                }, this);
 
-    });
-  }
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['click ' + this.options.button.remove] = function (event) {
+                    event.stopPropagation();
+                    self._removeItem($(event.currentTarget));
+                };
+
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['keyup ' + this.options.item.qty] = function (event) {
+                    self._showItemButton($(event.target));
+                };
+
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['change ' + this.options.item.qty] = function (event) {
+                    self._showItemButton($(event.target));
+                };
+
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['click ' + this.options.item.button] = function (event) {
+                    event.stopPropagation();
+                    self._updateItemQty($(event.currentTarget));
+                };
+
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['focusout ' + this.options.item.qty] = function (event) {
+                    self._validateQty($(event.currentTarget));
+                };
+
+                this._on(this.element, events);
+                this._calcHeight();
+            },
+
+        });
+    }
 });
